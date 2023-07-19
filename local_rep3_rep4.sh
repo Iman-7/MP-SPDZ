0ce45254af6b63061c6f3a24826af1f9fbd61234
Iman Attia <iman.elsadany@aucegypt.edu>
2023-07-19 19:35:27 +0000

    running computations local

---
+++ local_rep3_rep4.sh
@@ -0,0 +1,7 @@
+for name in bench_add_A bench_mul_A bench_equality_A bench_inequality_A bench_xor_A bench_and_A  # bench_and_B bench_xor_B bench_mul_B bench_add_B  bench_equality_B  bench_inequality_B # 
+do 
+
+    ./local_rep3.sh $name
+    ./local_rep4.sh $name
+
+done
---
+++ local_rep4.sh
@@ -0,0 +1,17 @@
+echo 1 2 3 4 > Player-Data/Input-P0-0
+echo 5 6 7 8 > Player-Data/Input-P1-0
+echo 9 10 11 12 > Player-Data/Input-P2-0
+echo 13 14 15 16 > Player-Data/Input-P3-0
+
+mpcfile=${1}
+
+mkdir -p results
+touch ./results/${mpcfile}_local_rep4.txt
+#Testing with the rep4 protocoal
+
+for ROWS in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304
+do  
+    python inputgen.py "./Programs/Source/${mpcfile}.mpc" $ROWS >> ./results/${mpcfile}_local_rep4.txt 
+    Scripts/compile-run.py -E rep4-ring ${mpcfile} >> ./results/${mpcfile}_local_rep4.txt
+    echo "----------------------------------------------" >> ./results/${mpcfile}_local_rep4.txt
+done
